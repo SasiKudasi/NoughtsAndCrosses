f743faff63feceebb505bfee45aa224eec1bf585
mallista <sasha.mittal@yandex.ru>
2025-03-22 19:54:25 +0300

    add todo

--- src/main/java/org/noughtsandcrosses/domain/model/GameModel.java
+++ src/main/java/org/noughtsandcrosses/domain/model/GameModel.java
@@ -4,9 +4,9 @@ import java.util.UUID;
 
 public class GameModel {
     private UUID id;
-    private int [][] gameField;
+    private int [] gameField;
 
-    public GameModel(UUID id, int[][] gameField) {
+    public GameModel(UUID id, int[] gameField) {
         this.id = id;
         this.gameField = gameField;
     }
